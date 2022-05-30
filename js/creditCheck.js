1263d0fe843c67f53bf17bca517228f48b9c962c
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 15:50:21 -0400

    finished js

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,24 @@
-exports.creditCheck = function(str) {
+exports.creditCheck = function(str){
+    const arr = str.split('')
+    let j = 0;
+    let sum = 0
+    for(let i = arr.length -1; i>=0; i--){
+        if(j%2 === 1)
+            arr[i]*=2
+        j++
+
+        if(arr[i]>=10)
+            arr[i]=(arr[i]%10)+((arr[i]-(arr[i]%10))/10)
+
+        sum+=parseInt(arr[i],10)
+    }
+
+    if(sum%10===0)
+        return("The number is valid!")
+
+    return("The number is invalid!")
 }
+
+//console.log(creditCheck('5541808923795240'))
+
+
